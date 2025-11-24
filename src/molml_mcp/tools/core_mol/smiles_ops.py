0c3ddfa10eb35cb12bd7d189ac5abde9a6f1b9fb
derekvantilborg <dwvantilborg@gmail.com>
2025-11-24 14:24:28 +0100

    smiles canonicalization as a list

--- src/molml_mcp/tools/core_mol/smiles_ops.py
+++ src/molml_mcp/tools/core_mol/smiles_ops.py
@@ -1,8 +1,14 @@
 from rdkit.Chem import MolFromSmiles, MolToSmiles
 
-def canonicalize_smiles(smiles: str) -> str: 
+def canonicalize_smiles(smiles: list[str]) -> list[str]: 
     """ Convert a SMILES string to its canonical form. """
-    mol = MolFromSmiles(smiles)
-    if mol is None:
-        raise ValueError("Invalid SMILES string")
-    return MolToSmiles(mol, canonical=True)
+
+    canonic = []
+    for smi in smiles:
+        mol = MolFromSmiles(smi)
+        if mol is None:
+            raise ValueError(f"Invalid SMILES string {smi}")
+        
+        canonic.append(MolToSmiles(mol, canonical=True))
+        
+    return canonic
