1845515a1402c05d96d91b061c40c58b6dbbdb82
derekvantilborg <dwvantilborg@gmail.com>
2025-12-01 14:46:14 +0100

    add canonicalize_smiles_dataset to server

--- src/molml_mcp/server.py
+++ src/molml_mcp/server.py
@@ -3,6 +3,7 @@ from mcp.server.fastmcp import FastMCP
 # All tools we want to expose via the MCP server
 from molml_mcp.tools.core import get_all_dataset_tools
 from molml_mcp.tools import canonicalize_smiles
+from molml_mcp.tools.core_mol.cleaning import canonicalize_smiles_dataset
 
 # create an MCP server 
 mcp = FastMCP("molml-mcp") 
@@ -11,7 +12,7 @@ mcp = FastMCP("molml-mcp")
 for tool_func in get_all_dataset_tools():
     mcp.add_tool(tool_func)
 
-# Add tools
+# Add molecular tools
 mcp.add_tool(canonicalize_smiles)
-# mcp.add_tool(canonicalize_smiles_dataset)
+mcp.add_tool(canonicalize_smiles_dataset)
 
