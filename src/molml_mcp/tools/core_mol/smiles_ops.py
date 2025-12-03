8bb5d2713ab3a107c500dd7db87d17828339be14
derekvantilborg <dwvantilborg@gmail.com>
2025-12-03 11:57:09 +0100

    All worker functions are now put here

--- src/molml_mcp/tools/core_mol/smiles_ops.py
+++ src/molml_mcp/tools/core_mol/smiles_ops.py
@@ -2,15 +2,6 @@ from rdkit.Chem import MolFromSmiles, MolToSmiles
 from molml_mcp.infrastructure.logging import loggable
 
 
-@loggable
-def canonicalize_smiles(smiles: list[str]) -> list[str]: 
-    """ Convert a SMILES string to its canonical form. Failed conversions are treated as None. Return canonicalized SMILES."""
-
-    canonic, comment = _canonicalize_smiles(smiles)
-
-    return canonic
-
-
 def _canonicalize_smiles(smiles: list[str]) -> tuple[list[str], list[str]]: 
     """ Convert a SMILES string to its canonical form. Failed conversions are treated as None. Return both canonical SMILES and comments."""
 
@@ -33,3 +24,42 @@ def _canonicalize_smiles(smiles: list[str]) -> tuple[list[str], list[str]]:
             comment.append(f"Failed: {str(e)}")
 
     return canonic, comment
+
+
+
+def _remove_pattern(smiles: list[str], smarts_pattern: str) -> tuple[list[str], list[str]]: 
+    """ Remove some pattern from a SMILES string using the specified SMARTS.
+
+    :param smiles: SMILES string
+    :param smarts_pattern: SMARTS pattern
+    :return: cleaned SMILES without pattern, comments
+    """
+    from rdkit.Chem.SaltRemover import SaltRemover
+
+    remover = SaltRemover(defnData=smarts_pattern)
+
+    new_smi, comment = [], []
+    for smi in smiles:
+
+        if '.' not in smi:
+            new_smi.append(smi)
+            comment.append("Passed")
+            continue
+
+        mol = MolFromSmiles(smi)
+
+        if mol is None:
+            new_smi.append(None)
+            comment.append("Failed: Invalid SMILES string")
+            continue
+
+        try:
+            new_smi = Chem.MolToSmiles(remover.StripMol(mol))
+            new_smi.append(new_smi)
+            comment.append("Passed")
+        except Exception as e:
+            new_smi.append(None)
+            comment.append(f"Failed: {str(e)}")
+
+    return new_smi, comment
+
