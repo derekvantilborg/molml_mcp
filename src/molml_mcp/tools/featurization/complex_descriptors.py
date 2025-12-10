82c2d97a2e5d58bfc2b739ff092587564b2119f4
derekvantilborg <dwvantilborg@gmail.com>
2025-12-10 11:06:05 +0100

    rdkit descriptors

--- src/molml_mcp/tools/featurization/complex_descriptors.py
+++ src/molml_mcp/tools/featurization/complex_descriptors.py
@@ -1,12 +1,14 @@
 
 from molml_mcp.tools.featurization.supported.ecfps import smiles_to_ecfp_dataset
 from molml_mcp.tools.featurization.supported.maccs import smiles_to_maccs_dataset
+from molml_mcp.tools.featurization.supported.rdkit import smiles_to_rdkit_fp_dataset
 
 
 def get_all_complex_descriptor_tools():
     """Return a list of all complex descriptor tools for MCP registration."""
     return [
         smiles_to_ecfp_dataset,
-        smiles_to_maccs_dataset
+        smiles_to_maccs_dataset,
+        smiles_to_rdkit_fp_dataset
     ]
 
