946d5537d8a683f440201a3eeb1fab8d9280aa5b
derekvantilborg <dwvantilborg@gmail.com>
2025-12-10 10:10:08 +0100

    added tools to mcp server

--- src/molml_mcp/server.py
+++ src/molml_mcp/server.py
@@ -1,14 +1,17 @@
 from mcp.server.fastmcp import FastMCP
 
 # All tools we want to expose via the MCP server
+from molml_mcp.infrastructure.resources import get_all_resources_tools
+
 from molml_mcp.tools.core import get_all_dataset_tools
 from molml_mcp.tools.core_mol import get_all_cleaning_tools
-from molml_mcp.tools.core_mol.descriptors import list_rdkit_descriptors, calculate_descriptors
 from molml_mcp.tools.core_mol.visualize import smiles_to_acs1996_png, smiles_grid_to_acs1996_png
 from molml_mcp.tools.core_mol.smiles_ops import enumerate_stereo_isomers_smiles
 from molml_mcp.tools.core_mol.substructure_matching import get_all_substructure_matching_tools
-from molml_mcp.infrastructure.resources import get_all_resources_tools
 from molml_mcp.tools.core_mol.data_splitting import random_split_dataset
+
+from molml_mcp.tools.featurization.simple_descriptors import list_rdkit_descriptors, calculate_simple_descriptors
+from molml_mcp.tools.featurization.complex_descriptors import get_all_complex_descriptor_tools
 
 # create an MCP server 
 mcp = FastMCP("molml-mcp") 
@@ -34,7 +37,9 @@ mcp.add_tool(enumerate_stereo_isomers_smiles)
 
 # Add descriptor tools
 mcp.add_tool(list_rdkit_descriptors)
-mcp.add_tool(calculate_descriptors)
+mcp.add_tool(calculate_simple_descriptors)
+for tool_func in get_all_complex_descriptor_tools():
+    mcp.add_tool(tool_func)
 
 # Add visualization tools
 mcp.add_tool(smiles_to_acs1996_png)
