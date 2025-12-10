da5fa3ee7e11e1852c4bd37a6c30c5fdaa04846f
derekvantilborg <dwvantilborg@gmail.com>
2025-12-10 11:03:12 +0100

    support for maccs

--- src/molml_mcp/tools/featurization/complex_descriptors.py
+++ src/molml_mcp/tools/featurization/complex_descriptors.py
@@ -1,10 +1,12 @@
 
 from molml_mcp.tools.featurization.supported.ecfps import smiles_to_ecfp_dataset
+from molml_mcp.tools.featurization.supported.maccs import smiles_to_maccs_dataset
 
 
 def get_all_complex_descriptor_tools():
     """Return a list of all complex descriptor tools for MCP registration."""
     return [
-        smiles_to_ecfp_dataset
+        smiles_to_ecfp_dataset,
+        smiles_to_maccs_dataset
     ]
 
