8f27421e3119ae70f6c0c6f52336fc6cb84ed583
derekvantilborg <dwvantilborg@gmail.com>
2026-01-28 09:42:07 +0100

    added CATS as a tool

--- src/molml_mcp/tools/featurization/complex_descriptors.py
+++ src/molml_mcp/tools/featurization/complex_descriptors.py
@@ -2,6 +2,7 @@
 from molml_mcp.tools.featurization.supported.ecfps import smiles_to_ecfp_dataset
 from molml_mcp.tools.featurization.supported.maccs import smiles_to_maccs_dataset
 from molml_mcp.tools.featurization.supported.rdkit import smiles_to_rdkit_fp_dataset
+from molml_mcp.tools.featurization.supported.cats import smiles_to_cats_dataset
 
 
 def get_all_complex_descriptor_tools():
@@ -9,6 +10,7 @@ def get_all_complex_descriptor_tools():
     return [
         smiles_to_ecfp_dataset,
         smiles_to_maccs_dataset,
-        smiles_to_rdkit_fp_dataset
+        smiles_to_rdkit_fp_dataset,
+        smiles_to_cats_dataset
     ]
 
